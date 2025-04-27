a97586a3c60becbff2bcf3c52ac0ee326b06ea62
varad <varad.code19@gmail.com>
2025-04-27 14:35:59 +0000

    added crt effect

---
+++ utils/sitconf.js
@@ -0,0 +1,38 @@
+const sitetitle = 'Extraquadrata V4';
+const sitedesc = "Extraquadrata V4";
+
+const herotitle = 'Extraquadrata V4';
+const herodesc = 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+const events = [
+    {
+        name: 'Site Incroyable',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        name: 'Another Event',
+        description: 'This is a description for another event.'
+    },
+    {
+        name: 'Third Event',
+        description: 'This is a description for the third event.'
+    },
+    {
+        name: 'Fourth Event',
+        description: 'This is a description for the fourth event.'
+    },
+    {
+        name: 'Fifth Event',
+        description: 'This is a description for the fifth event.'
+    },
+    {
+        name: 'Sixth Event',
+        description: 'This is a description for the sixth event.'
+    },
+    {
+        name: 'Seventh Event',
+        description: 'This is a description for the seventh event.'
+    },
+];
+
+export { events, sitetitle, sitedesc, herotitle, herodesc };
