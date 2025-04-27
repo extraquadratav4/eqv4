c9febce9c42414f948da7127c04d90aa017f24eb
varad <varad.code19@gmail.com>
2025-04-27 17:40:55 +0000

    heading animation

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,13 +1,19 @@
-import { herotitle, herodesc } from "@/utils/sitconf";
+import { herodesc } from "@/utils/sitconf";
 import NavBar from "../nav";
+import { TypewriterEffect } from "../typewriter";
 
 export default function Hero() {
+  const words_head = [
+    { text: "Extraquadrata", className: "text-8xl font-bold uppercase" },
+    { text: "V4", className: "text-8xl font-bold uppercase" },
+  ]
   return (
     // <section className="h-screen background-image backdrop-brightness-50 bg-cover bg-center bg-[url('/static_final.jpg')]">
     <section className="h-screen background-image bg-cover bg-center">
       <div className="piece output flex flex-col items-center justify-center h-screen backdrop-brightness-50 font-mono animate-(--textflicker)">
         <NavBar />
-        <h1 className="text-8xl font-bold uppercase">{herotitle}</h1>
+        {/* <h1 className="text-8xl font-bold uppercase">{herotitle}</h1> */}
+        <TypewriterEffect words={words_head} className="text-8xl font-bold uppercase" cursorClassName="hidden" />
         <p className="mt-4 text-lg text-center max-w-2xl">
           {herodesc}
         </p>
