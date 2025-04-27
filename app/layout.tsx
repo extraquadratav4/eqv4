f6770ced57ec8e13912329257a4bedc4694eebe1
varad <varad.code19@gmail.com>
2025-04-27 17:00:27 +0000

    hero -> full crt

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+import "./crt.css";
 import { sitetitle, sitedesc } from "@/utils/sitconf";
 import NavBar from "@/components/nav";
 
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -2,13 +2,16 @@ import { herotitle, herodesc } from "@/utils/sitconf";
 
 export default function Hero() {
   return (
-    <section className="h-screen background-image backdrop-brightness-50 bg-cover bg-center bg-[url('/static_final.jpg')]">
-      <div className="flex flex-col items-center justify-center h-screen backdrop-brightness-50 font-mono animate-(--textflicker)">
+    // <section className="h-screen background-image backdrop-brightness-50 bg-cover bg-center bg-[url('/static_final.jpg')]">
+    <section className="h-screen background-image bg-cover bg-center">
+      <div className="piece output flex flex-col items-center justify-center h-screen backdrop-brightness-50 font-mono animate-(--textflicker)">
         <h1 className="text-8xl font-bold uppercase">{herotitle}</h1>
         <p className="mt-4 text-lg text-center max-w-2xl">
           {herodesc}
         </p>
       </div>
+      <div className="piece scanlines noclick"></div>
+      <div className="piece glow noclick"></div>
     </section>
   );
 }
