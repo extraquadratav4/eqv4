96034571997e6ac66bdd9af7be384c1770e04e80
varad <varad.code19@gmail.com>
2025-04-27 17:04:28 +0000

    nav font

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full flex content-end items-center justify-end p-4 z-50 shadow-md">
+    <nav className="fixed top-0 left-0 w-full flex content-end items-center justify-end p-4 z-50 shadow-md font-mono text-xl">
       <ul className="flex space-x-4">
         <li>
           <Link href="/" className="hover:text-gray-400">
