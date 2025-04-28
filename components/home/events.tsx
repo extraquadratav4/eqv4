31f0e52e9c6e74508949cdd35f73dab5c56dc9a4
varad <varad.code19@gmail.com>
2025-04-28 08:15:37 +0000

    hotfix

--- components/home/events.tsx
+++ components/home/events.tsx
@@ -9,23 +9,25 @@ export default function EventList() {
     { text: "details", className: "text-4xl" },
   ]
   return (
-    <section className="font-mono output min-h-screen bg-[#001408]">
-      <div className="mt-5">
-        <TypewriterEffect words={eventdetailhead} />
-      </div>
-      <div className="mt-10 grid md:grid-cols-2 gap-10">
-        {events.map((eventinf, key) =>
-          <div 
-            key={key} 
-            className={cn(
-              key % 3 == 0 ? "md:col-span-2 text-center" : "",
-              key == 2 || key == 5 || key == 8 ? "text-right" : "",
-              "transition-all border p-5 hover:border-4 shadow-2xl shadow-green-950/50 output"
-            )}>
+    <section className="font-mono flex flex-col justify-center output min-h-screen bg-[#001408]">
+      <div className="flex flex-col items-center justify-center">
+        <div className="mt-5">
+          <TypewriterEffect words={eventdetailhead} />
+        </div>
+        <div className="mt-10 max-w-6xl grid md:grid-cols-2 gap-10">
+          {events.map((eventinf, key) =>
+            <div
+              key={key}
+              className={cn(
+                key % 3 == 0 ? "md:col-span-2 text-center" : "",
+                key == 2 || key == 5 || key == 8 ? "text-right" : "",
+                "transition-all border p-5 hover:border-4 shadow-2xl shadow-green-950/50 output"
+              )}>
               <p className="font-bold text-2xl">{eventinf.name}</p>
               <p>{eventinf.description}</p>
-          </div>
-        )}
+            </div>
+          )}
+        </div>
       </div>
     </section>
   )
