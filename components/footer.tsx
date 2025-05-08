0c0573f1625d51378f5d50fc499dcb75f88a3fdb
varad <varad.code19@gmail.com>
2025-05-08 11:53:06 +0000

    quirk

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <hr />
       <div className="mx-auto text-center p-5 max-w-7xl">
         <p className="text-sm">© 2024 DPSK Bhopal Headways Tech Club. All rights reserved.</p>
-        <p className="text-sm">Made with ❤️ by the Headways Team</p>
+        <p className="text-sm">Made by the Headways Team (fastersmt did it)</p>
       </div>
     </footer>
   );
