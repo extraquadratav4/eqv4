7c2643b2cb3464248fab24946dd34714f188a13a
extraquadratav4 <eqver.iv@gmail.com>
2025-07-28 06:43:17 +0000

    borchure

--- components/home/moreinf.tsx
+++ components/home/moreinf.tsx
@@ -18,8 +18,8 @@ export default function MoreInfo() {
         </div>
 
         {/* map snipper + brochure */}
-        <div className="flex mb-5 justify-center content-center align-middle">
-          <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3668.3422716734694!2d77.39607657437197!3d23.15770481127364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c44a073baa9e9%3A0x631bb328a32d81f4!2sDelhi%20Public%20School%20Kolar%20Road%2C%20Bhopal!5e0!3m2!1sen!2sin!4v1745810413456!5m2!1sen!2sin" width="400" height="500" allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="mt-5 rounded"></iframe></div>
+        <div className="flex flex-col mb-5 justify-center content-center align-center">
+          <div className=""><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3668.3422716734694!2d77.39607657437197!3d23.15770481127364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c44a073baa9e9%3A0x631bb328a32d81f4!2sDelhi%20Public%20School%20Kolar%20Road%2C%20Bhopal!5e0!3m2!1sen!2sin!4v1745810413456!5m2!1sen!2sin" width="400" height="500" allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="mt-5 rounded"></iframe></div>
         </div>
       </div>
     </section>
